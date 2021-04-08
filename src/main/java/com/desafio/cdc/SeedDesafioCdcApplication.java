14c908e5b927867cc441190f6855c322d3354d15
Romulo Ferreira Paiva <romulofpaiva@gmail.com>
2021-04-07 22:15:55 -0300

    Setup do projeto

---
+++ src/main/java/com/desafio/cdc/SeedDesafioCdcApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.cdc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeedDesafioCdcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeedDesafioCdcApplication.class, args);
+	}
+
+}
